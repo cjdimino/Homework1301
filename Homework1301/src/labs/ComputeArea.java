928de5396937173c6c3ef182b3bf8d35668cc956
cd02675 <cd02675@CEIT2552212X025.ad.georgiasouthern.edu>
2014-05-20 10:59:28 -0400

    lab 1

--- Homework1301/src/labs/ComputeArea.java
+++ Homework1301/src/labs/ComputeArea.java
@@ -7,7 +7,27 @@ public class ComputeArea {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		double radius;
+		double area;
+		
+		//Assign Radius
+		radius = 20;
+		
+		//Compute Area
+		area = radius * radius * 3.14159;
+		
+		System.out.println("The area of a circle with radius " + radius + " is " + area + ".");
+		
+		//Assign Radius
+		radius = 10;
+		
+		//Compute Area
+		area = radius * radius * 3.14159;
+		
+		System.out.println("The area of a circle with radius " + radius + " is " + area + ".");
+		
+		
 	}
 
 }
