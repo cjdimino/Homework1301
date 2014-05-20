85b022be45191ee50c9aa44fa91ac949f6e1c4ec
cd02675 <cd02675@CEIT2552212X025.ad.georgiasouthern.edu>
2014-05-20 11:05:28 -0400

    hw1

--- Homework1301/src/homework/HW1.java
+++ Homework1301/src/homework/HW1.java
@@ -1,5 +1,7 @@
 package homework;
 
+import java.util.Scanner;
+
 public class HW1 {
 
 	/**
@@ -7,6 +9,28 @@ public class HW1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		final double pi = 3.14159;
+		Scanner input = new Scanner(System.in);
+		
+		double radius;
+		double area;
+		double length;
+		double volume;
+		
+		
+		System.out.println("Enter a radius: ");
+		radius = input.nextDouble();
+		System.out.println("Enter a length: ");
+		length = input.nextDouble();
+		
+		area = radius * radius * pi;
+		volume = area * length;
+		
+		System.out.println("A cylinder with a radius of " + radius + " and a length of " + length + " has a volume of " + volume);
+		
+		
+		
 
 	}
 
