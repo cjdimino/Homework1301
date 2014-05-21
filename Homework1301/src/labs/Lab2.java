a30ded68b8d55a4dde253c60540f3f416f847386
cd02675 <cd02675@CEIT2552212X025.ad.georgiasouthern.edu>
2014-05-21 11:39:55 -0400

    Lab 2

---
+++ Homework1301/src/labs/Lab2.java
@@ -0,0 +1,28 @@
+package labs;
+
+import java.util.Scanner;
+
+public class Lab2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double meter;
+		double foot;
+		
+		Scanner input = new Scanner(System.in);
+		
+		foot = input.nextDouble();
+		meter = .305*foot;
+		
+		System.out.println("" + meter + " Meters");
+		
+		
+		
+
+	}
+
+}
