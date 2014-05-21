4a7e7aead95b7c01ca65d8d9fedc194ba36ca606
cd02675 <cd02675@CEIT2552212X025.ad.georgiasouthern.edu>
2014-05-21 10:16:24 -0400

    finished hw2

--- Homework1301/src/homework/Hw2.java
+++ Homework1301/src/homework/Hw2.java
@@ -15,8 +15,28 @@ public class Hw2 {
 		double temperature;
 		boolean isRaining;
 		Scanner input = new Scanner(System.in);
+		boolean doHomework = false;
 		
 		System.out.println("How many days until the assignment is due?");
+		days = input.nextInt();
+		System.out.println("How many assignments are due in all classes?");
+		assignments = input.nextInt();
+		System.out.println("What is the current temperature?");
+		temperature = input.nextDouble();
+		System.out.println("Is it raining?");
+		isRaining = input.nextBoolean();
+		
+		if( days < 5)
+			doHomework = true;
+		if(assignments > 4)
+			doHomework = true;
+		if(isRaining && temperature < 50.0)
+			doHomework = true;
+		
+		if(doHomework)
+			System.out.println("You should speed up");
+		else
+			System.out.println("Take your time");
 		
 		
 
