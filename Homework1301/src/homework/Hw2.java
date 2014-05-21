96c4cc1f60ae70a9b814c1b26cd39802a7e3e85c
cd02675 <cd02675@CEIT2552212X025.ad.georgiasouthern.edu>
2014-05-21 10:09:16 -0400

    Created hw2

---
+++ Homework1301/src/homework/Hw2.java
@@ -0,0 +1,25 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Hw2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int days;
+		int assignments;
+		double temperature;
+		boolean isRaining;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("How many days until the assignment is due?");
+		
+		
+
+	}
+
+}
